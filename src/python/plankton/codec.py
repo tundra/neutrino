ff1e6c965ed384645752c153efb99725f2e62799
Christian Plesner Hansen <c7n@p5r.org>
2013-06-09 11:21:52 +1000

    Added objects and circular references in plankton.

--- src/python/plankton/codec.py
+++ src/python/plankton/codec.py
@@ -21,6 +21,8 @@ class DataOutputStream(object):
 
   def __init__(self):
     self.bytes = bytearray()
+    self.object_index = {}
+    self.object_offset = 0
 
   # Writes a value to the stream.
   def write_object(self, obj):
@@ -37,12 +39,14 @@ class DataOutputStream(object):
     elif t == dict:
       self._add_byte(_MAP_TAG)
       self._encode_map(obj)
-    elif obj == None:
+    elif obj is None:
       self._add_byte(_NULL_TAG)
-    elif obj == True:
+    elif obj is True:
       self._add_byte(_TRUE_TAG)
-    elif obj == False:
+    elif obj is False:
       self._add_byte(_FALSE_TAG)
+    else:
+      self._write_object(obj)
 
   # Writes a naked unsigned int32.
   def _encode_uint32(self, value):
@@ -75,6 +79,24 @@ class DataOutputStream(object):
       self.write_object(k)
       self.write_object(v)
 
+  def _write_object(self, obj):
+    if obj in self.object_index:
+      self._add_byte(_REFERENCE_TAG)
+      offset = self.object_offset - self.object_index[obj] - 1
+      self._encode_uint32(offset)
+    else:
+      name = obj.__class__.__name__
+      desc = _DESCRIPTORS.get(name, None)
+      if desc is None:
+        raise Exception(obj)
+      index = self.object_offset
+      self.object_offset += 1
+      self._add_byte(_OBJECT_TAG)
+      self.object_index[obj] = -1
+      self.write_object(desc.get_header())
+      self.object_index[obj] = index
+      self.write_object(desc.get_payload(obj))
+
   # Adds a single byte to the stream.
   def _add_byte(self, byte):
     self.bytes.append(byte)
@@ -86,6 +108,8 @@ class DataInputStream(object):
   def __init__(self, bytes):
     self.bytes = bytes
     self.cursor = 0
+    self.object_index = {}
+    self.object_offset = 0
 
   # Reads the next value from the stream.
   def read_object(self):
@@ -104,6 +128,10 @@ class DataInputStream(object):
       return True
     elif tag == _FALSE_TAG:
       return False
+    elif tag == _OBJECT_TAG:
+      return self._decode_object()
+    elif tag == _REFERENCE_TAG:
+      return self._decode_reference()
     else:
       raise Exception(tag)
 
@@ -149,11 +177,89 @@ class DataInputStream(object):
       result[k] = v
     return result
 
+  # Reads a naked object from the stream.
+  def _decode_object(self):
+    index = self.object_offset
+    self.object_offset += 1
+    self.object_index[index] = None
+    header = self.read_object()
+    desc = _DESCRIPTORS.get(header, None)
+    if desc is None:
+      raise Exception(header)
+    instance = desc.new_instance()
+    self.object_index[index] = instance
+    payload = self.read_object()
+    desc.apply_payload(instance, payload)
+    return instance
+
+  # Reads a raw object reference from the stream.
+  def _decode_reference(self):
+    offset = self._decode_uint32()
+    index = self.object_offset - offset - 1
+    return self.object_index[index]
+
   # Reads a single byte from the stream.
   def _get_byte(self):
     result = self.bytes[self.cursor]
     self.cursor += 1
     return result
+
+
+_DESCRIPTORS = {}
+
+
+# A description of how to serialize and deserialize values of a particular
+# type.
+class ObjectDescriptor(object):
+
+  def __init__(self, klass):
+    self.klass = klass
+    self.fields = None
+
+  # Returns the header to use when encoding the given instance
+  def get_header(self):
+    return self.klass.__name__
+
+  # Returns the data payload to use when encoding the given instance.
+  def get_payload(self, instance):
+    fields = {}
+    for field in self._field_names():
+      fields[field] = getattr(instance, field)
+    return fields
+
+  # Returns the list of names of serializable instance fields.
+  def _field_names(self):
+    if self.fields is None:
+      init = self.klass.__init__
+      self.fields = list(init._plankton_fields_)
+      self.fields.reverse()
+    return self.fields
+
+  # Creates a new empty instance of this type of object.
+  def new_instance(self):
+    return (self.klass)()
+
+  # Sets the data in the given instance based on the given payload.
+  def apply_payload(self, instance, payload):
+    for (name, value) in payload.iteritems():
+      setattr(instance, name, value)
+
+# Marks the given class as serializable.
+def serializable(klass):
+  name = klass.__name__
+  _DESCRIPTORS[name] = ObjectDescriptor(klass)
+  return klass
+
+
+# When used to decorate __init__ indicates that a serializable class has the
+# given field.
+def field(name):
+  def callback(fun):
+    if not hasattr(fun, '_plankton_fields_'):
+      fun._plankton_fields_ = []
+    fun._plankton_fields_.append(name)
+    return fun
+  return callback
 
 
 # Encodes an object as plankton, returning a byte array.
--- tests/python/plankton/test_codec.py
+++ tests/python/plankton/test_codec.py
@@ -1,19 +1,39 @@
 #!/usr/bin/python
 
 
-from plankton import codec
+import plankton
 import unittest
+
+
+@plankton.serializable
+class Pair(object):
+
+  @plankton.field("first")
+  @plankton.field("second")
+  def __init__(self, first=None, second=None):
+    self.first = first
+    self.second = second
+
+  def __eq__(self, that):
+    return (self is that) or (self.first == that.first) and (self.second == that.second)
+
+  def __str__(self):
+    return "(%s, %s)" % (self.first, self.second)
 
 
 class PlanktonTest(unittest.TestCase):
 
   def check_transcoding(self, value):
-    raw_encoded = codec.serialize(value)
-    raw_decoded = codec.deserialize(raw_encoded)
+    raw_encoded = plankton.serialize(value)
+    raw_decoded = plankton.deserialize(raw_encoded)
     self.assertEquals(value, raw_decoded)
-    base64_encoded = codec.base64encode(value)
-    base64_decoded = codec.base64decode(base64_encoded)
+    base64_encoded = plankton.base64encode(value)
+    base64_decoded = plankton.base64decode(base64_encoded)
     self.assertEquals(value, base64_decoded)
+
+  def transcode(self, value):
+    raw_encoded = plankton.serialize(value)
+    return plankton.deserialize(raw_encoded)
 
   def test_primitive_encoding(self):
     test = self.check_transcoding
@@ -43,6 +63,21 @@ class PlanktonTest(unittest.TestCase):
     test({'a': 1, 'b': 2})
     test({})
 
+  def test_object_encoding(self):
+    test = self.check_transcoding
+    test(Pair(1, 2))
+    test(Pair(Pair(1, 2), Pair(3, 4)))
+    pb = Pair("foo")
+    pb.second = pb
+    pa = self.transcode(pb)
+    self.assertEquals("foo", pa.first)
+    self.assertTrue(pa is pa.second)
+    ab = [pb, pb, pb]
+    aa = self.transcode(ab)
+    self.assertEquals("foo", aa[0].first)
+    self.assertTrue(aa[0] is aa[0].second)
+    self.assertTrue(aa[0] is aa[1])
+    self.assertTrue(aa[0] is aa[2])
 
 if __name__ == '__main__':
   runner = unittest.TextTestRunner(verbosity=0)
