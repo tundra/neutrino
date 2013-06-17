c5d049a017fae172252f6b1ad2413135568246f7
Christian Plesner Hansen <c7n@p5r.org>
2013-06-17 17:01:01 +1000

    Made test failure output much more helpful.

--- tests/c/test.h
+++ tests/c/test.h
@@ -6,35 +6,51 @@
 
 
 // Aborts exception, signalling an error.
-extern void fail(const char *error, const char *file, int line);
-
-// Fails unless the condition is true.
-#define ASSERT_TRUE(COND) do { \
-  if (!(COND)) \
-    fail("Assertion failed.", __FILE__, __LINE__); \
-} while (0)
-
-// Fails unless the condition is false.
-#define ASSERT_FALSE(COND) ASSERT_TRUE(!(COND))
+extern void fail(const char *file, int line, const char *fmt, ...);
 
 // Fails unless the two values are equal.
-#define ASSERT_EQ(A, B) ASSERT_TRUE((A) == (B))
+#define ASSERT_EQ(A, B) do {                                         \
+  if (!((A) == (B)))                                                 \
+    fail(__FILE__, __LINE__, "Assertion failed: %s == %s.", #A, #B); \
+} while (false)
+
+// Fails unless the two given strings are equal.
+#define ASSERT_STREQ(A, B) do {                                               \
+  string_t *__a__ = (A);                                                      \
+  string_t *__b__ = (B);                                                      \
+  if (!(string_equals(__a__, __b__)))                                         \
+    fail(__FILE__, __LINE__, "Assertion failed: %s == %s.\n  Expected: %s\n  Found: %s", \
+        #A, #B, __a__->chars, __b__->chars);                                  \
+} while (false)
+
+// Fails unless the condition is true.
+#define ASSERT_TRUE(COND) ASSERT_EQ(COND, true)
+
+// Fails unless the condition is false.
+#define ASSERT_FALSE(COND) ASSERT_EQ(COND, false)
 
 // Fails unless the given value is within the given domain.
 #define ASSERT_DOMAIN(vdDomain, EXPR) \
-  ASSERT_EQ(vdDomain, get_value_domain(EXPR))
+ASSERT_CLASS(value_domain_t, vdDomain, EXPR, get_value_domain)
+
+// Fails unless the given value is within the given family.
+#define ASSERT_FAMILY(ofFamily, EXPR) \
+ASSERT_CLASS(object_family_t, ofFamily, EXPR, get_object_family)
+
+// Fails unless the given value is a signal of the given type.
+#define ASSERT_SIGNAL(scCause, EXPR) \
+ASSERT_CLASS(signal_cause_t, scCause, EXPR, get_signal_cause)
+
+#define ASSERT_CLASS(class_t, cExpected, EXPR, get_class) do {               \
+  class_t __class__ = get_class(EXPR);                                       \
+  if (__class__ != cExpected)                                                \
+    fail(__FILE__, __LINE__, "Assertion failed: %s(%s) == %s.\n  Found: %i", \
+        #get_class, #EXPR, #cExpected, __class__);                           \
+} while (false)
 
 // Fails if the given value is a signal.
 #define ASSERT_SUCCESS(EXPR) \
   ASSERT_FALSE(vdSignal == get_value_domain(EXPR))
-
-// Fails unless the given value is within the given family.
-#define ASSERT_FAMILY(ofFamily, EXPR) \
-  ASSERT_TRUE(in_family(ofFamily, EXPR))
-
-// Fails unless the given value is a signal of the given type.
-#define ASSERT_SIGNAL(scCause, EXPR) \
-  ASSERT_TRUE(is_signal(scCause, EXPR))
 
 // Declares a new string_t variable and initializes it with the given contents.
 #define DEF_STRING(name, contents) \
