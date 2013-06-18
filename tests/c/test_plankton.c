a55d86c6499080d0eca9249edf5803fecb696182
Christian Plesner Hansen <c7n@p5r.org>
2013-06-18 19:49:47 +1000

    Added plankton handling of singletons.

--- tests/c/test_plankton.c
+++ tests/c/test_plankton.c
@@ -3,22 +3,28 @@
 
 // Encodes and decodes a plankton value and checks that the result is the
 // same as the input.
-static void check_plankton(value_t value) {
-  // Create a runtime.
-  runtime_t runtime;
-  ASSERT_SUCCESS(runtime_init(&runtime, NULL));
+static void check_plankton(runtime_t *runtime, value_t value) {
   // Encode and decode the value.
-  value_t encoded = plankton_serialize(&runtime, value);
-  value_t decoded = plankton_deserialize(&runtime, encoded);
+  value_t encoded = plankton_serialize(runtime, value);
+  value_t decoded = plankton_deserialize(runtime, encoded);
   ASSERT_EQ(value, decoded);
-  // Clean up.
-  runtime_dispose(&runtime);
 }
 
 TEST(plankton, simple) {
-  check_plankton(new_integer(0));
-  check_plankton(new_integer(1));
-  check_plankton(new_integer(-1));
-  check_plankton(new_integer(65536));
-  check_plankton(new_integer(-65536));
+  runtime_t runtime;
+  ASSERT_SUCCESS(runtime_init(&runtime, NULL));
+
+  // Integers
+  check_plankton(&runtime, new_integer(0));
+  check_plankton(&runtime, new_integer(1));
+  check_plankton(&runtime, new_integer(-1));
+  check_plankton(&runtime, new_integer(65536));
+  check_plankton(&runtime, new_integer(-65536));
+
+  // Singletons
+  check_plankton(&runtime, runtime_null(&runtime));
+  check_plankton(&runtime, runtime_bool(&runtime, true));
+  check_plankton(&runtime, runtime_bool(&runtime, false));
+
+  runtime_dispose(&runtime);
 }
