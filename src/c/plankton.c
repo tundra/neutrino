5ba3d90e5bd62e2e99f44c8998a64d9fbe365d2e
Christian Plesner Hansen <c7n@p5r.org>
2013-06-18 16:57:29 +1000

    Added encoding of integers.

--- src/c/plankton.c
+++ src/c/plankton.c
@@ -5,6 +5,17 @@
 #include <string.h>
 
 
+static const byte_t kInt32Tag = 0;
+static const byte_t kStringTag = 1;
+static const byte_t kArrayTag = 2;
+static const byte_t kMapTag = 3;
+static const byte_t kNullTag = 4;
+static const byte_t kTrueTag = 5;
+static const byte_t kFalseTag = 6;
+static const byte_t kObjectTag = 7;
+static const byte_t kReferenceTag = 8;
+static const byte_t kEnvironmentTag = 9;
+
 // --- B y t e   b u f f e r ---
 
 // Buffer for building a block of bytes incrementally.
@@ -65,10 +76,50 @@ static void byte_buffer_flush(byte_buffer_t *buf, blob_t *blob_out) {
 
 // --- S e r i a l i z e ---
 
+// Encodes an unsigned 32-bit integer.
+static value_t encode_uint32(uint32_t value, byte_buffer_t *buf) {
+  while (value > 0x7F) {
+    // As long as the value doesn't fit in 7 bits chop off 7 bits and mark
+    // them with a high 1 to indicate that there's more coming.
+    byte_t part = value & 0x7F;
+    byte_buffer_append(buf, part | 0x80);
+    value >>= 7;
+  }
+  byte_buffer_append(buf, value);
+  return success();
+}
+
+// Zig-zag encodes a 32-bit signed integer.
+static value_t encode_int32(int32_t value, byte_buffer_t *buf) {
+  uint32_t zig_zag = (value << 1) ^ (value >> 31);
+  return encode_uint32(zig_zag, buf);
+}
+
+value_t integer_serialize(value_t value, byte_buffer_t *buf) {
+  CHECK_DOMAIN(vdInteger, value);
+  byte_buffer_append(buf, kInt32Tag);
+  // TODO: deal with full size integers, for now just trap loss of data.
+  int64_t int_value = get_integer_value(value);
+  int32_t trunc = (int32_t) int_value;
+  CHECK_EQ(int_value, (int64_t) trunc);
+  return encode_int32(trunc, buf);
+}
+
+value_t value_serialize(value_t data, byte_buffer_t *buf) {
+  switch (get_value_domain(data)) {
+    case vdInteger:
+      return integer_serialize(data, buf);
+    default:
+      UNREACHABLE();
+      return new_signal(scUnsupportedBehavior);
+  }
+}
+
 value_t plankton_serialize(runtime_t *runtime, value_t data) {
   // Write the data to a C heap blob.
   byte_buffer_t buf;
   byte_buffer_init(&buf, NULL);
+  TRY(value_serialize(data, &buf));
   blob_t buffer_data;
   byte_buffer_flush(&buf, &buffer_data);
   // Allocate the blob object to hold the result.
