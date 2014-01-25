984615a28cc34c68538d7634cad40cd3ae4d8021
Christian Plesner Hansen <c7n@p5r.org>
2014-01-25 14:17:50 +0100

    Added topic logging.

--- src/c/interp.c
+++ src/c/interp.c
@@ -160,6 +160,7 @@ value_t run_stack(runtime_t *runtime, value_t stack) {
   interpreter_state_load(&state, &frame);
   while (true) {
     opcode_t opcode = (opcode_t) read_next_short(&state);
+    TOPIC_INFO(Interpreter, "Opcode: %i", opcode);
     switch (opcode) {
       case ocPush: {
         value_t value = read_next_value(&state);
