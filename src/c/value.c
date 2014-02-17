56635dc0033780b1b0abf271f1b22dcc8e0983b4
Christian Plesner Hansen <c7n@p5r.org>
2014-02-17 14:56:39 +0100

    Self-review

--- src/c/value.c
+++ src/c/value.c
@@ -2395,11 +2395,12 @@ ACCESSORS_IMPL(Ambience, ambience, acInFamilyOpt, ofModuleFragment,
 
 value_t ambience_validate(value_t self) {
   VALIDATE_FAMILY(ofAmbience, self);
+  VALIDATE_FAMILY_OPT(ofModuleFragment, get_ambience_present_core_fragment(self));
   return success();
 }
 
 void get_ambience_layout(value_t value, object_layout_t *layout) {
-  // An ambience has no value fields.
+  // An ambience has one non-value field.
   object_layout_set(layout, kAmbienceSize, OBJECT_FIELD_OFFSET(1));
 }
 
