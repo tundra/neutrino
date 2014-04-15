f0e56e1484dd604decea9de9efcbb49916ff64b0
Christian Plesner Hansen <c7n@p5r.org>
2014-04-15 16:31:37 +0200

    Self-review

--- tools/travis-install.sh
+++ tools/travis-install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright 2014 the Neutrino authors (see AUTHORS).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+## Installs any dependencies used when building on travis-ci.
 
 set -v -e
 
