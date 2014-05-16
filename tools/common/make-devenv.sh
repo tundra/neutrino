45fcc078a0fd90ced6e67547ea92b3b7391df207
Christian Plesner Hansen <c7n@p5r.org>
2014-05-16 09:18:38 +0200

    Trying to pull out devenv setup code.

---
+++ tools/common/make-devenv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright 2014 the Neutrino authors (see AUTHORS).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+## Sets up a build environment.
+
+set -v -e
+
+sudo apt-get install python-pip
+
+sudo pip install virtualenv
+
+virtualenv devenv
+
+./ensure-mkmk.sh
