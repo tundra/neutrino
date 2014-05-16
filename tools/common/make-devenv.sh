c8b94e473053d5d5bc0b691e860d0197991098ed
Christian Plesner Hansen <c7n@p5r.org>
2014-05-16 09:48:36 +0200

    Don't land within mkmk.

--- tools/common/make-devenv.sh
+++ tools/common/make-devenv.sh
@@ -19,12 +19,11 @@ if ! which mkmk > /dev/null; then
     echo "Trying to create devenv."
     virtualenv devenv
   fi
-  . devenv/bin/activate
   if [ ! -d mkmk ]; then
     echo "Trying to check out mkmk from github."
     git clone https://github.com/goto-10/mkmk.git mkmk
   fi
   echo "Trying to install python package."
-  cd mkmk
-  python setup.py develop
+  . devenv/bin/activate
+  $(cd mkmk && python setup.py develop)
 fi
