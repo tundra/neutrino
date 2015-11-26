38b324e1112607a41b1b7170919f217178ed743e
Christian Plesner Hansen <plesner@t.undra.org>
2015-11-26 09:07:31 +0100

    Moved macro stuff to tclib.

--- src/c/utils-inl.h
+++ src/c/utils-inl.h
@@ -6,64 +6,6 @@
 #define _UTILS_INL
 
 #include "utils.h"
-
-// --- V a r i a d i c   m a c r o s ---
-
-// Utility that picks out the correct count from the arguments passed by VA_ARGC.
-#define __VA_ARGC_PICK_COUNT__(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, COUNT, ...) COUNT
-
-// Rather than call the macro directly we call it through this one to ensure
-// that the __VA_ARGS__ macro is expanded into all its tokens rather than
-// substituted as a single token, which MSVC happily does.
-#define __CALL_VA_ARGC_PICK_COUNT_WITH__(TUPLE) __VA_ARGC_PICK_COUNT__ TUPLE
-
-// Expands to the number of arguments given as var args. Note that this macro
-// does _not_ work with 0 arguments.
-#define VA_ARGC(...) __CALL_VA_ARGC_PICK_COUNT_WITH__((__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
-
-// Expands the given function for each element in the var args. The
-// implementation of this is insane, plus there is a fixed limit on how many
-// arguments are allowed, but it's really useful.
-//
-// The way it works is that it pairs each argument with an action that expands
-// for that argument. The actions are picked out in reverse order so the action
-// to terminate is last, the actions to continue before it.
-#define FOR_EACH_VA_ARG(F, ...)                                                \
-  __E16__(__FOR_EACH_VA_ARG_HELPER__(F, __VA_ARGS__,                           \
-      __C__, __C__, __C__, __C__, __C__, __C__, __C__, __C__, __C__, __C__, __T__))
-
-// Kicks things off by invoking the first action which, if necessary, will take
-// care of invoking the remaining ones.
-#define __FOR_EACH_VA_ARG_HELPER__(F, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, ACTION, ...)\
-  DEFER(ACTION)()(F, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, __VA_ARGS__)
-
-// The __EN__ macro expands N times. This is because the preprocessor doesn't
-// allow macros to expand themselves recursively so instead we generate an
-// intermediate result with unexpanded recursive calls and then use this
-// set of macros to manually expand repeatedly.
-#define __E1__(...) __VA_ARGS__
-#define __E2__(...) __E1__(__E1__(__VA_ARGS__))
-#define __E4__(...) __E2__(__E2__(__VA_ARGS__))
-#define __E16__(...) __E4__(__E4__(__VA_ARGS__))
-
-// To be completely honest I don't understand what's going on here.
-#define EMPTY
-#define DEFER(...) __VA_ARGS__ EMPTY
-
-// Trampoline for continuing the expansion.
-#define __C__() __DO_C__
-
-// Trampoline for ending the expansion.
-#define __T__() __DO_T__
-
-// Apply the callback to the first argument, shift down the arguments and
-// actions, and continue recursively.
-#define __DO_C__(F, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, NEXT, ...)         \
-  F(_0)                                                                        \
-  DEFER(NEXT)()(F, _1, _2, _3, _4, _5, _6, _7, _8, _9, NEXT, __VA_ARGS__)
-
-// Stop expanding.
-#define __DO_T__(...)
-
+#include "utils/macro-inl.h"
 
 #endif // _UTILS_INL
--- tests/c/test_utils.cc
+++ tests/c/test_utils.cc
@@ -6,7 +6,6 @@
 BEGIN_C_INCLUDES
 #include "alloc.h"
 #include "runtime.h"
-#include "utils-inl.h"
 #include "value-inl.h"
 END_C_INCLUDES
 
@@ -286,43 +285,6 @@ TEST(utils, base64_encode) {
       84, 104, 105, 110, 103, 115, 32, 102, 97, 108, 108, 32, 97, 112, 97, 114,
       116, 59, 32, 116, 104, 101, 32, 99, 101, 110, 116, 114, 101, 32, 99, 97,
       110, 110, 111, 116, 32, 104, 111, 108, 100, 59);
-}
-
-// Record that this macro has been invoked with the given argument.
-#define RECORD(N) record[count++] = N;
-
-TEST(utils, for_each_va_arg) {
-  int count = 0;
-  int record[8];
-
-  FOR_EACH_VA_ARG(RECORD, 1, 4, 6, 9, 34, 54, 2, 3);
-  ASSERT_EQ(8, count);
-  ASSERT_EQ(record[0], 1);
-  ASSERT_EQ(record[1], 4);
-  ASSERT_EQ(record[2], 6);
-  ASSERT_EQ(record[3], 9);
-  ASSERT_EQ(record[4], 34);
-  ASSERT_EQ(record[5], 54);
-  ASSERT_EQ(record[6], 2);
-  ASSERT_EQ(record[7], 3);
-
-  count = 0;
-  FOR_EACH_VA_ARG(RECORD, 6, 5, 4);
-  ASSERT_EQ(3, count);
-  ASSERT_EQ(record[0], 6);
-  ASSERT_EQ(record[1], 5);
-  ASSERT_EQ(record[2], 4);
-}
-
-TEST(utils, va_argc) {
-  ASSERT_EQ(1, VA_ARGC(a));
-  ASSERT_EQ(2, VA_ARGC(a, b));
-  ASSERT_EQ(3, VA_ARGC(a, b, c));
-  ASSERT_EQ(4, VA_ARGC(a, b, c, d));
-  ASSERT_EQ(5, VA_ARGC(a, b, c, d, e));
-  ASSERT_EQ(6, VA_ARGC(a, b, c, d, e, f));
-  ASSERT_EQ(7, VA_ARGC(a, b, c, d, e, f, g));
-  ASSERT_EQ(8, VA_ARGC(a, b, c, d, e, f, g, h));
 }
 
 TEST(utils, byte_buffer_cursor) {
